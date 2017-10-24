5e3132533083c332ebe21482479ababc8bb38fc2
Igor Vavilin <igorvv.vn@gmail.com>
2017-10-24 20:07:47 +0400

    Add assets

--- core/src/com/vavilinigor/studygame/utils/Constants.java
+++ core/src/com/vavilinigor/studygame/utils/Constants.java
@@ -22,7 +22,7 @@ public class Constants {
     public static final float GROUND_DENSITY = 0f;
 
     public static final float RUNNER_X = 2f;
-    public static final float RUNNER_Y = GROUND_Y+GROUND_HEIGHT+1f;
+    public static final float RUNNER_Y = GROUND_Y+GROUND_HEIGHT;
     public static final float RUNNER_WIDTH = 1f;
     public static final float RUNNER_HEIGHT = 2f;
     public static final float RUNNER_GRAVITY_SCALE = 3f;
@@ -31,7 +31,7 @@ public class Constants {
 
     public static float RUNNER_DENSITY = 0.5f;
     public static final float RUNNER_DODGE_X = 2f;
-    public static final float RUNNER_DODGE_Y = 2.5f;
+    public static final float RUNNER_DODGE_Y = 1.5f;
     public static final Vector2 RUNNER_JUMPING_LINEAR_IMPULSE = new Vector2(0,13f);
     public static final float RUNNER_HIT_ANGULAR_IMPULSE = 10f;
 
