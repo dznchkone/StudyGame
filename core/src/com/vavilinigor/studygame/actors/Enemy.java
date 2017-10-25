f4e52bbfbb038439f59f3814ba7f17677bac7f25
Igor Vavilin <igorvv.vn@gmail.com>
2017-10-25 12:17:37 +0300

    Done

--- core/src/com/vavilinigor/studygame/actors/Enemy.java
+++ core/src/com/vavilinigor/studygame/actors/Enemy.java
@@ -1,8 +1,14 @@
 package com.vavilinigor.studygame.actors;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.vavilinigor.studygame.box2d.EnemyUserData;
 import com.vavilinigor.studygame.box2d.UserData;
+import com.vavilinigor.studygame.utils.Constants;
 
 /**
  * Created by ODSnew on 17.10.2017.
@@ -10,8 +16,19 @@ import com.vavilinigor.studygame.box2d.UserData;
 
 public class Enemy extends GameActor {
 
+    private Animation animation;
+    private float stateTime;
+
     public Enemy(Body body) {
         super(body);
+        TextureAtlas textureAtlas = new TextureAtlas(Constants.CHARACTERS_ATLAS_PATH);
+        TextureRegion[] runningFrames = new TextureRegion[getUserData().getTextureRegions().length];
+        for (int i=0; i<getUserData().getTextureRegions().length;i++){
+            String path = getUserData().getTextureRegions()[i];
+            runningFrames[i] = textureAtlas.findRegion(path);
+        }
+        animation = new Animation(0.1f,runningFrames);
+        stateTime = 0f;
     }
 
     @Override
@@ -23,5 +40,13 @@ public class Enemy extends GameActor {
     public void act(float delta){
         super.act(delta);
         body.setLinearVelocity(getUserData().getLinearVelocity());
+    }
+
+    @Override
+    public void draw(Batch batch,float parentAlpha){
+        super.draw(batch, parentAlpha);
+        stateTime += Gdx.graphics.getDeltaTime();
+        batch.draw((TextureRegion) animation.getKeyFrame(stateTime,true),(screenRectangle.x-(screenRectangle.width*0.1f)),
+                screenRectangle.y,screenRectangle.width*1.2f,screenRectangle.height*1.1f);
     }
 }
